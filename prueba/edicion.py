996a78f465fa8ff04118b87f1b5fd4d8b9ed7209
Zanter7 <125771138+Zanter7@users.noreply.github.com>
2023-10-04 23:50:49 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   prueba/edicion.py
    
     Changes not staged for commit:
    	modified:   prueba/crear.py
    	modified:   prueba/delta.db
    	modified:   prueba/edicion.py
    
     Untracked files:
    	Clases/

--- prueba/edicion.py
+++ prueba/edicion.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 # conexion a la base de datos
 database = sqlite3.connect("prueba/delta.db")
@@ -7,9 +8,24 @@ database = sqlite3.connect("prueba/delta.db")
 #ejecuta comando insert a la DB articulos en los campos descripcion y precio, con ? indica la posicion donde se inserta
 # inserta una tupla a la posicion
 
-database.execute("insert into articulos(descripcion,precio) values (?,?)", ("naranjas", 23.50))
-database.execute("insert into articulos(descripcion,precio) values (?,?)", ("peras", 34))
-database.execute("insert into articulos(descripcion,precio) values (?,?)", ("bananas", 25))
+
+def letrazar():
+    abecedario =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+    'v', 'w', 'x', 'y', 'z']
+
+    nivel = random.randint(3,10)
+
+    nombre = ""
+
+    for i in range(nivel):
+        nombre+= abecedario[random.randint(0,len(abecedario))-1]
+        
+    return nombre
+
+
+for i in range(20):
+    database.execute("insert into articulos(descripcion,precio) values (?,?)", (letrazar(), random.randint(0,500)))
+
 
 
 database.commit() # guarda cambios
