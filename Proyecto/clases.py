f7f2591335da38f6a10d3be4a2eb852bdf4704b3
Zanter7 <125771138+Zanter7@users.noreply.github.com>
2023-10-05 20:52:16 +0000

    Changes to be committed: 	modified:   Proyecto/BaseDatosClientes.db 	modified:   Proyecto/clases.py

--- Proyecto/clases.py
+++ Proyecto/clases.py
@@ -47,15 +47,55 @@ class cliente:
     @staticmethod
     def Consultar(DB,idcliente):
 
-        DB.cursor.execute("SELECT * FROM Clientes WHERE Identificacion = ?" , (idcliente))
+        DB.cursor.execute("SELECT * FROM Clientes WHERE Identificacion = ?" , (idcliente,)) # "," idcliente es importante
 
         infocliente = DB.cursor.fetchone()
 
-        print(infocliente)
+        return infocliente
 
 
 
 
 db = BaseDeDatos() #Crea instancia de la base de datos (Solucion al problema de crear conexion cada vez que se opera)
 
-cliente.Consultar(db,123)
+cliente.Consultar(db,1011322256)
+
+
+while True: #ciclo de prueba
+
+    print("bienvenido a la DB de clientes")
+    print("Que desea hacer ?")
+    print("Digite: ")
+    print("1. Para crear un nuevo cliente")
+    print("2. para editar la direccion de un cliente")
+    print("3. Para consultar sobre un cliente")
+    print("4. para salir")
+
+    control = int(input())
+
+    if control>4:
+        print("Numero no disponible")
+        continue
+
+    if control == 1:
+        a = input("ID Cliente: ")
+        b = input("nombre Cliente: ")
+        c = input("Apellido Cliente: ")
+        d = input("direccion Cliente: ")
+        e = input("telefono Cliente: ")
+        f = input("correo Cliente: ")
+        clienteN = cliente(a,b,c,d,e,f)
+        clienteN.CrearCliente(db)
+    
+    if control == 2:
+        cliente.ActualizarDir(db,input("ingrese identificacion a editar numero cliente:"), input("ingrese nueva direccion:"))
+        print("hecho")
+
+    if control == 3:
+        print(cliente.Consultar(db,input("id cliente a consultar:")))
+    
+    if control == 4:
+        db.guardar()
+        db.CerrarConexion()
+        print("desconectao")
+        break
